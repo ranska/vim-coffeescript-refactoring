36b5f1ce81c001b50ac620e6d605afaf130f9d4a
Pierre-Alexandre Piarulli <go@rubyrock.me>
2015-01-10 01:04:54 +0100

    simplify spec

--- spec/plugin/extract_method_spec.rb
+++ spec/plugin/extract_method_spec.rb
@@ -6,6 +6,7 @@ require 'spec_helper'
 #      :RExtractMethod
 #      <leader>rem
 
+=begin
 describe "extract method" do
   let(:filename) { 'extract_method.coffee' }
   #  Scenario: Extract one line assignment into a new method
@@ -56,6 +57,55 @@ class Foo
     four = two + two
     five = add(two, three)
     six = five + one
+    EOF
+  end
+end
+=end
+WITH_PAUSES = false
+WITH_END_PAUSES = true
+
+#
+# NOTE test first extract method 
+# without params
+#
+describe "extract method" do
+  let(:filename) { 'extract_method.coffee' }
+  #  Scenario: Extract one line assignment into a new method
+  #    Given I have the following code:
+  specify "with params" do
+    set_file_contents <<-EOF
+class Foo
+  method_one: ->
+    a = 1
+    b = 2
+    c = 3
+    EOF
+    vim.command 'set number'
+    vim.command 'hi Visual  guifg=#FFFF00 guibg=#003322 gui=none'
+    vim.edit filename
+    vim.feedkeys '\\<esc>'
+    sleep 2 if WITH_PAUSES
+    vim.write
+    vim.select_lines 4, 5
+    sleep 2 if WITH_PAUSES
+    #vim.feedkeys ':call CRExtractMethod()\\<CR>'
+    vim.type ':call CRExtractMethod()'
+    vim.feedkeys '\\<CR>'
+    sleep 2 if WITH_PAUSES
+    # And I fill in the parameter "add"
+    vim.feedkeys 'add\\<CR>'
+    sleep 5 if WITH_PAUSES or WITH_END_PAUSES
+
+    #    Then I should see:
+    assert_file_contents <<-EOF 
+class Foo
+  add: ->
+    b = 2
+    c = 3
+
+  method_one: ->
+    a = 1
+    @add()
     EOF
   end
 end
